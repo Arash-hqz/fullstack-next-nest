71af5c2bbab5f5286db1e02a78055a6c4c6f98e3
Arash-hqz <ghaffariarash369@gmail.com>
2025-10-07 19:53:48 +0330

    feat: Add 'use client' directive to admin pages for client-side rendering

--- frontend/src/app/admin/articles/[id]/page.tsx
+++ frontend/src/app/admin/articles/[id]/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useEffect, useState } from 'react'
 import { AdminLayout } from '../../../../components/layout/admin/layout'
 import { adminApi } from '../../../services/adminApi'
